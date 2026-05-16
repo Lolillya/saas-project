2a125263ef8bd0bcd89d7122601257b9def01b57
Lolillya <someonesusernam@gmail.com>
2026-05-16 08:26:34 +0800

    fix(gateway): correct dev cluster URL to port 5001; add local JWT key to dev config

--- src/ApiGateway/InventorySaas.Gateway/Program.cs
+++ src/ApiGateway/InventorySaas.Gateway/Program.cs
@@ -34,6 +34,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         };
     });
 
+
 builder.Services.AddAuthorization();
 
 builder.Services.AddRateLimiter(options =>
